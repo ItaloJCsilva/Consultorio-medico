0198ae4c9129ca1e714330d292343795b25d625b
Italo josé cavalcante da silva <85091764+ItaloJCsilva@users.noreply.github.com>
2022-07-10 23:55:08 -0300

    Add files via upload

---
+++ src/main/java/Interface/Model.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Interface;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+
+public class Model extends AbstractTableModel {
+    
+    ArrayList<Pacientes> pacientes = new ArrayList();
+
+    @Override
+    public int getRowCount() {
+         
+    }
+
+    @Override
+    public int getColumnCount() {
+         
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+         
+    }
+    
+}
